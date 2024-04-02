89057393386a42ca76c7f2b3311a9ef57c3188ce
Semnick <81602550+semniiick@users.noreply.github.com>
2024-04-02 08:08:39 -0300

    Add files via upload

---
+++ iniciar.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Ferramenta sendo iniciada...
+timeout /t 5 >nul
+node .
+pause
---
+++ instalar.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Instalando dependencias...
+npm install venom-bot axios worker_threads
+echo Instalacao concluida com sucesso.
+pause
